baae92e2a0a4db22b8d4364d62dd5f643ec0b9a6
Ibukun Ayomide-Baafog <ayomideracheal12@gmail.com>
2024-01-12 08:04:14 +0000

    Commit for onboarding screens 2 completion

---
+++ lib/onboarding_screen_1B.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'onboarding_screen_1A.dart';
+
+class onboarding1B extends StatefulWidget {
+  const onboarding1B({super.key});
+
+  @override
+  State<onboarding1B> createState() => _onboarding1BState();
+}
+
+class _onboarding1BState extends State<onboarding1B> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
